214ea9007e9b924254092d42367841cdcee131d1
zerograd <kyle.wilsonmccormack@gmail.com>
2017-04-30 20:42:13 -0400

    Changed CSS for post page

--- app/Http/Controllers/PostingController.php
+++ app/Http/Controllers/PostingController.php
@@ -9,15 +9,17 @@ use Response;
 class PostingController extends Controller
 {
     //
-    public function index($id = null){
+    public function index($id = null,$keywords = null){
     	$posting = DB::table('postings')
-    				->select('*')
-    				->where('id',$id)
+    				->select(DB::raw('postings.*'),DB::raw('companies.id AS companyID'),DB::raw('companies.company_name'))
+    				->leftJoin('companies',DB::raw('companies.id'),'=',DB::raw('postings.company_id'))
+    				->where(DB::raw('postings.id'),$id)
     				->first();
     	$data = array(
-    		'posting' => $posting
+    		'posting' => $posting,
+    		'keywords' => $keywords
 		);
-		
+
 		return view('posting')->with($data);
     }
 }
--- resources/views/posting.blade.php
+++ resources/views/posting.blade.php
@@ -49,6 +49,11 @@
           outline: 1px solid slategrey;
         }
 
+        
+        .no-hover:hover{
+            text-decoration: none;
+            color:black;
+        }
         </style>
     </head>
     <body>
@@ -73,13 +78,19 @@
                 </ul>
             </div>
         </div>
+        <div class="col-sm-12" style="display:block;float:left;">
+            <a href="{{URL::to('/')}}" class="no-hover"><h5 style="float:left;">Back to Search Tool</h5></a>
+        </div>
         <div class="container">
             <div  style="margin:50px 0;height:100%;border:1px solid #354886;border-radius: 10px;padding:10px;">
                 <div class="row" style="text-align: center;margin-top: 15px;">
                     <img class="center-block img-responsive" style="width: 200px;height: 155px;float: left;margin:0 40px;" src="{{URL::asset('/images/nasa.png')}}" alt="Company Photo">
-                    <div class="row" style="text-align: left;">
+                    <div class="row" style="text-align: left;float:left;">
                         <h4>{{$posting->title}}</h4>
+                        <a href="{{route('company-get',$posting->companyID)}}" class="no-hover"><h5>{{$posting->company_name}}</h5></a>
+                        <p style="font-weight: bold">Location: {{$posting->location}}</p>
                         <p style="font-weight: bold">Keywords: {{$posting->keywords}}</p>
+                        <p style="font-weight: bold">REQUIRED EXPERIENCED: {{$posting->required_experience}}</p>
                         <p style="font-weight: bold">Posted: {{$posting->posted_date}}</p>
                         <button style="margin:0 auto" class="btn waves-effect waves-teal">Save this Job</button>
                         <button style="margin:0 auto" class="btn waves-effect waves-teal">Apply Now</button>
@@ -99,6 +110,17 @@
                 </div>
             </div>
         </div>
+        <input type="text" style="visibility: hidden" value="{{$keywords}}" id="searchkeywords" />
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            // function backToResults(){
+            //     var keywords = $('#searchkeywords').val();
+            //     $.post('{{route('submit-search')}}',{
+
+            //     },function(data){
+
+            //     });
+            // }
+        </script>
     </body>
 </html>
