137e80274cf91b46a10efd491cf64b87ab16c5f2
zerograd <kyle.wilsonmccormack@gmail.com>
2017-05-20 00:16:35 -0400

    New layout for welcome.blade.php

--- resources/views/layout/header-layout.blade.php
+++ resources/views/layout/header-layout.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-    <script src="https://use.fontawesome.com/ba4723fdf4.js"></script>
+    <!-- <script src="https://use.fontawesome.com/ba4723fdf4.js"></script> -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{URL::asset('/css/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 
         <title>
             @yield('title')
