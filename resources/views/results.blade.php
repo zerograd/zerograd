2120959b0cd08f9e8673ea49ad73f904c6049483
zerograd <kyle.wilsonmccormack@gmail.com>
2017-05-20 00:40:17 -0400

    New layout for welcome.blade.php

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -10,7 +10,7 @@
                                 <div class="row" style="margin-top:20px;">
                                     
                                     <h3 style="margin:0 40px;color:black;display:inline-block;float:left;">{{$posting->company_name}}</h3>
-                                    <h4 style="margin:0;display:inline-block;padding:5px;color:#FFFFFF;font-weight: bold;float:right;text-transform: uppercase;font-size:14px;background-color: {{ $badges[mt_rand(0,2)]}};">{{ $badges[mt_rand(0,2)]}};">
+                                    <h4 style="margin:0;display:inline-block;padding:5px;color:#FFFFFF;font-weight: bold;float:right;text-transform: uppercase;font-size:14px;background-color: {{ $badges[mt_rand(0,2)]}};">
                                             
                                             @if(isset($posting->status))
                                                 {{$posting->status}}
