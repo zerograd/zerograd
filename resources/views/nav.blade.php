8b250d97e4ce09b5d7d99ce71891bcb3e2eb9cd9
zerograd <kyle.wilsonmccormack@gmail.com>
2017-06-04 18:40:11 -0400

    Refactoring main page

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -9,20 +9,20 @@
        <a href="{{URL::to('/')}}"><h1 id="logo" style="margin:0;padding:15px; color:white;display:block;" class="text-xs-center">Zer<span class="zeroLogo">0</span>Grad</h1></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar" style="margin: 0;">
-      <div class="col-md-3 col-xs-12" style="padding: 0;">
+      <div class="col-md-3" style="padding: 0;">
         <ul class="navigation nav navbar-nav">
           <li><a href="{{URL::to('/about')}}" style="font-size:18px;">About</a></li>
           <li><a style="font-size:18px;">Contact Us</a></li>
         </ul>
       </div>
-      <div class="col-md-5 col-xs-12" style="padding: 0;">
+      <div class="col-md-6 col-xs-12" style="padding: 0;">
         <ul class="navigation nav navbar-nav col-xs-12">
           @if(!Session::has('user_id'))
-                    <li class="col-xs-4"><a href="{{URL::to('/login')}}"><button class="white-btn">LOGIN</button></a></li>
-                    <li class="col-xs-4"><a href="{{URL::to('/student-register')}}"><button class="white-btn">Register</button></a></li>
-                    <li class="col-xs-4"><a href="{{URL::to('/employer-register')}}"><button class="white-btn">Employer?</button></a></li>
+                    <li class="col-md-3 col-xs-4"><a href="{{URL::to('/login')}}"><button class="white-btn">LOGIN</button></a></li>
+                    <li class="col-md-3 col-xs-4"><a href="{{URL::to('/student-register')}}"><button class="white-btn">Register</button></a></li>
+                    <li class="col-md-3 col-xs-4"><a href="{{URL::to('/employer-register')}}"><button class="white-btn">Employer?</button></a></li>
                     @else
-                    <li><a href="{{URL::to('/student/home')}}"><button class="white-btn">{{Session::get('student_name')}}</button></a></li>
+                    <li><a href="{{URL::to('/student/home')}}"><button class="white-btn user-btn">{{Session::get('student_name')}}</button></a></li>
           @endif
         </ul>
       </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,8 +31,95 @@
                 </form>
             </div>
     </section>
-    
 
+    <section>
+        <!-- LATEST JOBS -->
+            <div id="latest-jobs" style="background-color:white;width:100%;height:1000px;padding:5px;">
+                <h4 class="text-center" style="margin: 50px 0 10px 0;text-transform: uppercase;color: black;">Latest</h4>
+                <div class="col-sm-12" style="text-align: center;">
+                    <h2 style="margin-bottom: 35px;font-weight: bold;font-size: 32px;">Recent jobs</h2>
+                    <div style="margin:5px auto;background-color:##23CCF3;width:80px;height:3px;">
+                    </div>
+                </div>
+                
+                    <div class="container-fluid">
+                        <ul>
+                        @foreach($postings as $posting)
+                        <a href="{{route('posting-get',['title' => $posting->title,'id' => $posting->id])}}" style="text-decoration: none;"><li class="recent-job col-md-6 col-xs-12">
+                            <div class="container-fluid" style="text-align: center;">
+                                    <img src="{{URL::asset('/images/google.png')}}" alt="company logo" title="">
+                                <div class="col-md-10 col-sm-11 col-xs-12">
+                                    <div class="row">
+                                        <h3 class="post-title">{{$posting->title}}</h3>
+                                        <h4 class="location">{{$posting->location}}</h4>
+                                    </div>   
+                                    <div class="row">
+                                        <h3 class="post-company-name">{{$posting->company_name}}</h3>
+                                        <h4 class="status" style="background-color: {{ $badges[mt_rand(0,2)]}};">
+                                            
+                                            @if(isset($posting->status))
+                                                {{$posting->status}}
+                                            @else
+                                                Not Specified
+                                            @endif
+                                        </h4>
+                                    </div>
+                                </div>
+                            </div>
+                            </li></a>
+                        @endforeach
+                        </ul>
+                        <div class="col-md-12" style="text-align: center;">
+                        <a href="{{URL::to('/search')}}"><button type="button" style="margin:0 auto;padding:10px 40px;font-weight: 600;letter-spacing: 1px;font-size: 18px"class="btn btn-info">BROWSE ALL JOBS</button></a>
+                        </div>
+                    </div>
+                    
+                
+            </div>
+    </section>
+    
+     <!-- Database info -->
+        <section>
+                 
+            <div id="database-info" style="background-color:white;width:100%;height:221px;text-align: center;margin-top:120px;">
+
+                <div class="container-fluid">
+                    <!-- Jobs -->
+                <div class="counter col-md-3 ">
+                    <div class="row" style="text-align: center;"><h2 >{{$sizeOfJobs}} + </h2>
+                    <div style="margin:20px auto;width:60px;height:2px;background-color:#23CCF3; "></div>
+                    </div>
+
+                    <div class="row"><h4>Jobs</h4></div>
+
+                </div>
+
+                <!-- Members -->
+                <div class="counter col-md-3">
+                    <div class="row" style="text-align: center;"><h2>{{$sizeOfMembers}} + </h2></div>
+                    <div style="margin:20px auto;width:60px;height:2px;background-color:#23CCF3; "></div>
+                    <div class="row"><h4>Members</h4></div>
+
+                </div>
+
+                <!-- Resumes -->
+                <div class="counter col-md-3">
+                    <div class="row" style="text-align: center;"><h2>{{$sizeOfResumes}} + </h2></div>
+                    <div style="margin:20px auto;width:60px;height:2px;background-color:#23CCF3; "></div>
+                    <div class="row"><h4>Resume</h4></div>
+
+                </div>
+
+                <!-- Companies -->
+                <div class="counter col-md-3">
+                    <div class="row" style="text-align: center;"><h2>{{$sizeOfCompanies}} + </h2></div>
+                    <div style="margin:20px auto;width:60px;height:2px;background-color:#23CCF3; "></div>
+                    <div class="row"><h4>Company</h4></div>
+
+                </div>
+                </div>
+            </div>
+        </section>
 
 
 
