4a2d330181c2339b2070e0d83ae0c6e952c2a272
zerograd <kyle.wilsonmccormack@gmail.com>
2017-07-11 13:55:56 -0400

    Latest commit for manage resumes

--- resources/views/layout/header-layout.blade.php
+++ resources/views/layout/header-layout.blade.php
@@ -22,7 +22,13 @@
         
         <link href="{{URL::asset('/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" type="text/css" href="{{URL::asset('/radios-to-slider/css/radios-to-slider.css')}}">
-        
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-2940368448899385",
+            enable_page_level_ads: true
+          });
+        </script>
         <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css"> -->
         
         
