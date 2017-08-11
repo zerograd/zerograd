fb2933e6457d80f165f84046a7ab0272ee8018e9
zerograd <kyle.wilsonmccormack@gmail.com>
2017-08-10 22:05:09 -0400

    Quick Fixes: Resume Builder

--- resources/views/templates/resume-template-7.blade.php
+++ resources/views/templates/resume-template-7.blade.php
@@ -8,7 +8,7 @@
 
 <link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Lato:400,300' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{URL::asset('theme/css/font-awesome.css')}}">
+<link rel="stylesheet" href="{{URL::asset('/css/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 <script src="{{URL::asset('theme/scripts/jquery-2.1.3.min.js')}}"></script>
 <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 <!--[if lt IE 9]>
@@ -21,7 +21,7 @@
 			height:100%;
 			margin:0;
 			font-family: 'Raleway', sans-serif;
-			background-color: #689a68;
+			background-color: #20BD88;
 		}
 
 		#Content {
@@ -35,24 +35,63 @@
 			padding: 20px;
 		}
 
+		#resume-name-div{
+			background-color: #4FDCAD;
+			text-align: center;
+			margin-bottom: 10px;
+		}
+
 		#leftColumn {
-			height:100%;
+			height:90%;
 			border-right:2px solid white;
 		}
 
 		#rightColumn {
+			height:90%;
+		}
 
+		.resume-name {
+			color:white;
+			font-size: 48px;
+		}
+
+		.right-section {
+			width: 100%;
+			padding: 20px;
+		}
+
+		.section-title {
+			color:white;
+			text-transform: uppercase;
+			font-weight: 600;
+		}
+		.section-title i {
+			margin:0 30px;
 		}
 	</style>
 </head>
 <body>
 	<div id="Content" class="container-fluid">
 		<div class="inner container-fluid">
+			<div id="resume-name-div" class="col-sm-12">
+				<h2 class="resume-name">Kyle Wilson-McCormack</h2>
+			</div>
 			<div id="leftColumn" class="col-sm-3">
 				
 			</div>
 			<div id="rightColumn" class="col-sm-9">
-				
+				<div class="right-section col-sm-12">
+					<h2 class="section-title">PROFILE&nbsp<i class="fa fa-user-o" aria-hidden="true"></i></h2>
+					<p class="section-content"></p>
+				</div>
+				<div class="right-section col-sm-12">
+					<h2 class="section-title">Work Experience<i class="fa fa-building" aria-hidden="true"></i></h2>
+					<p class="section-content"></p>
+				</div>
+				<div class="right-section col-sm-12">
+					<h2 class="section-title">Projects<i class="fa fa-tasks" aria-hidden="true"></i></h2>
+					<p class="section-content"></p>
+				</div>
 			</div>
 		</div>
 	</div>
